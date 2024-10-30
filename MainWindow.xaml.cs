929313d41d224a6287435498b090b8d9dda2b43d
Kr1pto-psix <agent.facit.06@bk.ru>
2024-10-30 16:06:36 +0300

    new Elements and половина темы

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,16 +19,39 @@ namespace Flow
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    public enum ThemeType
+    {
+        Dark =0,
+        Light =1
+    }
+
+    public enum CircleState
+    {
+        circle = 0,
+        half = 1,
+        fill =2
+    }
     public partial class MainWindow : Window
     {
+        private ThemeType themeType;
+        private bool start = false;
+
+        private CircleState circleState1 = CircleState.circle;
+        private CircleState circleState2 = CircleState.circle;
+        private CircleState circleState3 = CircleState.circle;
+        private CircleState circleState4 = CircleState.circle;
+
+
         public MainWindow()
         {
+
             InitializeComponent();
             exitBut.Visibility = Visibility.Hidden;
             desabledBtn1.Visibility = Visibility.Hidden;
             desabledBtn2.Visibility = Visibility.Hidden;
             backbtn.Visibility = Visibility.Hidden;
             grachBtn.Visibility = Visibility.Hidden;
+            this.themeType = ThemeType.Dark;
         }
 
 
@@ -82,6 +105,145 @@ namespace Flow
             desabledBtn2.Visibility = Visibility.Hidden;
             backbtn.Visibility = Visibility.Hidden;
             grachBtn.Visibility = Visibility.Hidden;
+        }
+
+        private void quitSettingsButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+
+        private void ChangeTheme(object sender, RoutedEventArgs e)
+        {
+            if (themeType == ThemeType.Dark)
+            {
+                themeType = ThemeType.Light;
+
+                if (circleState1 == CircleState.circle)
+                    this.cirlce1.Source = (BitmapImage)FindResource("darkCercle");
+                if (circleState1 == CircleState.fill)
+                    this.cirlce1.Source = (BitmapImage)FindResource("darkCercleFill");
+                if (circleState1 == CircleState.half)
+                    this.cirlce1.Source = (BitmapImage)FindResource("darkCerleHalf");
+
+
+                if (circleState2 == CircleState.circle)
+                    this.cirlce2.Source = (BitmapImage)FindResource("darkCercle");
+                if (circleState2 == CircleState.fill)
+                    this.cirlce2.Source = (BitmapImage)FindResource("darkCercleFill");
+                if (circleState2 == CircleState.half)
+                    this.cirlce2.Source = (BitmapImage)FindResource("darkCerleHalf");
+
+
+                if (circleState3 == CircleState.circle)
+                    this.cirlce3.Source = (BitmapImage)FindResource("darkCercle");
+                if (circleState3 == CircleState.fill)
+                    this.cirlce3.Source = (BitmapImage)FindResource("darkCercleFill");
+                if (circleState3 == CircleState.half)
+                    this.cirlce3.Source = (BitmapImage)FindResource("darkCerleHalf");
+
+                if (circleState4 == CircleState.circle)
+                    this.cirlce4.Source = (BitmapImage)FindResource("darkCercle");
+                if (circleState4 == CircleState.fill)
+                    this.cirlce4.Source = (BitmapImage)FindResource("darkCercleFill");
+                if (circleState4 == CircleState.half)
+                    this.cirlce4.Source = (BitmapImage)FindResource("darkCerleHalf");
+
+                this.MainWindowBorder.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                this.DragMover.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+                this.exitBut.Stroke = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                this.desabledBtn1.Stroke = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                this.desabledBtn2.Stroke = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+                this.FlowLabel.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+
+                this.backBtnImage.Source = (BitmapImage)FindResource("backLight");
+                this.grachBtnImage.Source = (BitmapImage)FindResource("darkGrach");
+                this.settingsBtnImage.Source = (BitmapImage)FindResource("blackSettings");
+
+                this.separator1.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                this.separator2.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+
+                this.minTimeBlock.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                this.hourTimeBlok.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+
+                this.popupBackground.Background = new SolidColorBrush(Color.FromArgb(204, 255, 255, 255));
+
+            }
+            else
+            {
+                themeType = ThemeType.Dark;
+
+                if (circleState1 == CircleState.circle)
+                    this.cirlce1.Source = (BitmapImage)FindResource("lightCercleEmpty");
+                if (circleState1 == CircleState.fill)
+                    this.cirlce1.Source = (BitmapImage)FindResource("lightCercleFill");
+                if (circleState1 == CircleState.half)
+                    this.cirlce1.Source = (BitmapImage)FindResource("lightCercleHalf");
+
+
+                if (circleState2 == CircleState.circle)
+                    this.cirlce2.Source = (BitmapImage)FindResource("lightCercleEmpty");
+                if (circleState2 == CircleState.fill)
+                    this.cirlce2.Source = (BitmapImage)FindResource("lightCercleFill");
+                if (circleState2 == CircleState.half)
+                    this.cirlce2.Source = (BitmapImage)FindResource("lightCercleHalf");
+
+
+                if (circleState3 == CircleState.circle)
+                    this.cirlce3.Source = (BitmapImage)FindResource("lightCercleEmpty");
+                if (circleState1 == CircleState.fill)
+                    this.cirlce3.Source = (BitmapImage)FindResource("lightCercleFill");
+                if (circleState3 == CircleState.half)
+                    this.cirlce3.Source = (BitmapImage)FindResource("lightCercleHalf");
+
+                if (circleState4 == CircleState.circle)
+                    this.cirlce4.Source = (BitmapImage)FindResource("lightCercleEmpty");
+                if (circleState4 == CircleState.fill)
+                    this.cirlce4.Source = (BitmapImage)FindResource("lightCercleFill");
+                if (circleState4 == CircleState.half)
+                    this.cirlce4.Source = (BitmapImage)FindResource("lightCercleHalf");
+
+                this.MainWindowBorder.Background = new SolidColorBrush(Color.FromRgb(40,42,42));
+                this.DragMover.Background = new SolidColorBrush(Color.FromRgb(40, 42, 42));
+
+
+                this.exitBut.Stroke = new SolidColorBrush(Color.FromRgb(45, 48, 47));
+                this.desabledBtn1.Stroke = new SolidColorBrush(Color.FromRgb(45, 48, 47));
+                this.desabledBtn2.Stroke = new SolidColorBrush(Color.FromRgb(45, 48, 47));
+                
+
+                this.FlowLabel.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+                this.backBtnImage.Source = (BitmapImage)FindResource("backDark");
+                this.grachBtnImage.Source = (BitmapImage)FindResource("lightGrach");
+                this.settingsBtnImage.Source = (BitmapImage)FindResource("lightSettings");
+
+                this.separator1.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                this.separator2.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+                this.minTimeBlock.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                this.hourTimeBlok.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+                
+
+                this.popupBackground.Background = new SolidColorBrush(Color.FromArgb(204, 40, 42, 42));
+            }
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            this.start = !this.start;
+            if (this.start)
+            {
+                this.StartButtonImage.Source = (BitmapImage)FindResource("pause");
+            }
+            else
+            {
+                this.StartButtonImage.Source = (BitmapImage)FindResource("play");
+            }
+            
+
         }
     }
 }
